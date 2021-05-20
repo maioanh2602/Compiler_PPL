e343126fbe443e66cbcc7ec1c85078de4d50e37b
sonicthinh2000 <58232543+sonicthinh2000@users.noreply.github.com>
2021-05-20 20:25:13 +0700

    Results of scanner

--- compiler/lexer.py
+++ compiler/lexer.py
@@ -14,11 +14,13 @@ class Lexer:
         self.lexer.add('INTEGER', r'-?\d+')
         self.lexer.add('STRING', r'(""".*""")|(".*")|(\'.*\')')
         self.lexer.add('BOOLEAN', r'true(?!\w)|false(?!\w)|True(?!\w)|False(?!\w)|TRUE(?!\w)|FALSE(?!\w)')
+
         # Mathematical Operators
         self.lexer.add('SUM', r'\+')
         self.lexer.add('SUB', r'\-')
         self.lexer.add('MUL', r'\*')
         self.lexer.add('DIV', r'\/')
+
         # Binary Operator
         self.lexer.add('AND', r'and(?!\w)')
         self.lexer.add('OR', r'or(?!\w)')
@@ -29,18 +31,22 @@ class Lexer:
         self.lexer.add('>', r'\>')
         self.lexer.add('<', r'\<')
         self.lexer.add('=', r'\=')
+
         # Statement
         self.lexer.add('IF', r'if(?!\w)')
         self.lexer.add('ELSE', r'else(?!\w)')
         self.lexer.add('NOT', r'not(?!\w)')
+
         # Semi Colon
         self.lexer.add(';', r'\;')
         self.lexer.add(',', r'\,')
+
         # Parenthesis
         self.lexer.add('(', r'\(')
         self.lexer.add(')', r'\)')
         self.lexer.add('{', r'\{')
         self.lexer.add('}', r'\}')
+
         # Function
         self.lexer.add('CONSOLE_INPUT', r'input')
         self.lexer.add('FUNCTION', r'function')
@@ -50,13 +56,13 @@ class Lexer:
         self.lexer.add('COS', r'cos')
         self.lexer.add('TAN', r'tan')
         self.lexer.add('POWER', r'pow')
+
         # Assignment
         self.lexer.add('LET', r'let(?!\w)')
         self.lexer.add('IDENTIFIER', "[a-zA-Z_][a-zA-Z0-9_]*")
+
         # Ignore spaces
         self.lexer.ignore('\s+')
-
-        # self.lexer.add('OPT_LINE', r'\n*')
 
     def build(self):
         return self.lexer.build()
