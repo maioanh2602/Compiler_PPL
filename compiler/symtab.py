a03cf37896bfe95d07e70a8bc1111fca26cfdfe4
william-nguyen128 <quannguyen.flagship@gmail.com>
2021-05-22 15:41:23 +0700

    Changes: 1) main.py: Added delarations and function calls to print Symbol Table 2) symtab.py: Changes in code to print out prettier table, also prints out Type of the Symbols 3) Symbol Tables folder: Changes in most of the pictures to better suit the case of the project

--- compiler/symtab.py
+++ compiler/symtab.py
@@ -4,10 +4,22 @@ class SymTab:
         self.functions = functions
 
     def draw(self):
-        data = self.variables
-        data.extend(self.functions)
+        data = []
 
-        print("Symbol")
-        print("--------------------")
-        for value in data:
-            print(value)
+        for var in self.variables.keys():
+            instance = []
+            instance.append(var)
+            instance.append('Number')
+            data.append(instance)
+        for func in self.functions.keys():
+            instance = []
+            instance.append(func)
+            instance.append('Function')
+            data.append(instance)
+
+        print("+" + '-'*22 + "+" + '-'*22 + "+" + '-'*22 + "+")
+        print("| {:<20} | {:<20} | {:<20} |".format('Symbol', 'Type', 'Scope'))
+        print("+" + '-'*22 + "+" + '-'*22 + "+" + '-'*22 + "+")
+        for res in data:
+            print("| {:<20} | {:<20} | {:<20} |".format(res[0], res[1], 'Global'))
+        print("+" + '-'*22 + "+" + '-'*22 + "+" + '-'*22 + "+")
